c5a4007b3ef6f911954f6c61b9f8dc4d960d0637
Leonard Garvey <lengarvey@gmail.com>
2012-11-07 20:28:52 +1100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lesson8::Application.config.secret_token = '7aececc596aeebde0fde2940ea56716608866f00ecf3d1a30035a0480af9d49a684f17ad97ae35197749d5403f0dad0ccba27727d6f77364f3cca08e4b86ec22'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Lesson8::Application.routes.draw do
+  get "cards/index"
+
+  get "pages/home"
+
+  # root :to => 'welcome#index'
+
+end
